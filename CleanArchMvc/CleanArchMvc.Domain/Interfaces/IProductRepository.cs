41efcfe4a06dad005839c8ed336fa4a0a15fc3cd
DevOne <devone.softwaresolutions@gmail.com>
2022-11-12 13:27:07 -0300

    create repository interfaces

---
+++ CleanArchMvc/CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchMvc.Domain.Entities;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetCategoriesAsync();
+        Task<Product> GetByIdAsync(int? id);
+        Task<Product> GetProductByCategoryAsync(int? id);
+
+        Task<Product> CreateAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> RemoveAsync(Product product);
+    }
+}
