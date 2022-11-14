090d68688fb45b81e03af9a69f28409926b5bcd3
Marcos Venicius <marcosdevelopment.business@gmail.com>
2022-11-14 18:58:10 -0300

    implement category service

---
+++ CleanArchMvc/CleanArchMvc.Application/Interfaces/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchMvc.Application.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Interfaces
+{
+    public interface IProductService
+    {
+        public Task<IEnumerable<ProductDTO>> GetProducts();
+        public Task<ProductDTO> GetById(int id);
+        public Task Add(ProductDTO productDto);
+        public Task Update(ProductDTO productDto);
+        public Task Remove(int? id);
+    }
+}
