efef27681d7ebc6f9646283f6d3f3c6d9bbe1579
Marcos Venicius <marcosdevelopment.business@gmail.com>
2022-11-14 19:26:16 -0300

    configure auto mapper DI

--- CleanArchMvc/CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
+++ CleanArchMvc/CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,5 +1,4 @@
 ﻿using CleanArchMvc.Domain.Entities;
-using CleanArchMvc.Infra.Data.EntitiesConfiguration;
 using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchMvc.Infra.Data.Context
@@ -17,12 +16,7 @@ namespace CleanArchMvc.Infra.Data.Context
         {
             base.OnModelCreating(modelBuilder);
 
-            // apply configuration by reflection
-            // modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
-
-            // apply configuration manualy
-            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
-            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
         }
     }
 }
