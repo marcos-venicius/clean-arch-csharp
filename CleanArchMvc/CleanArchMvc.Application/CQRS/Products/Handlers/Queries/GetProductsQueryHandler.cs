788766c9e02e52eddce1d85bdf0ab88bbb566696
Marcos Venicius <marcosdevelopment.business@gmail.com>
2022-11-19 09:36:42 -0300

    implement CQRS to product entity

---
+++ CleanArchMvc/CleanArchMvc.Application/CQRS/Products/Handlers/Queries/GetProductsQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using CleanArchMvc.Application.CQRS.Products.Queries;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+
+using MediatR;
+
+namespace CleanArchMvc.Application.CQRS.Products.Handlers.Queries
+{
+    public class GetProductsQueryHandler : IRequestHandler<GetProductsQuery, IEnumerable<Product>>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public GetProductsQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IEnumerable<Product>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            var products = await _productRepository.GetProductsAsync();
+
+            return products;
+        }
+    }
+}
---
+++ CleanArchMvc/CleanArchMvc.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CleanArchMvc.Application.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message) { }
+    }
+}
